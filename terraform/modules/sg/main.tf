79b6caeff99d9833667f360a9c0920803838608d
Gormery Kombo Wanjiru <gormerykw@student.uia.no>
2024-05-02 02:01:36 +0200

    become: true

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -26,6 +26,14 @@ resource "aws_security_group" "sg" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
+
+  ingress {
+    description      = "coolify"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
   
   egress {
     from_port        = 0
